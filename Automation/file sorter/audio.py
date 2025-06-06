c5471e240f1aa11f88f4209b91f0707d3a827b05
Emad_Haji99 <emadhajiakbari1234@gmail.com>
2025-06-06 20:05:29 +0330

    fix the bug and add message

--- Automation/file sorter/audio.py
+++ Automation/file sorter/audio.py
@@ -12,7 +12,9 @@ def serial_movie_founder():
 # finding the algorithem for sort the videose 
     while True:
         for index,(dirpath, dirnames, filenames) in enumerate(os.walk(main_path)):
-            if index==0:     
+            if len(filenames)==0:
+                break
+            if index==0:      
                 for file_index,file in enumerate(filenames):
                     root,ext= os.path.splitext(file)
                     base_deteremination=root.lower()[0:8]
@@ -24,6 +26,10 @@ def serial_movie_founder():
                     change_formt=File_list[0].lower()[0:8]
                     if change_formt==base_deteremination:
                         File_list.append(file)
+#empty file 
+        if len(filenames)==0:
+                print("completley sorted")
+                break
 # movies 
         if len(File_list)==1:                                                  
             shutil.move(os.path.join(main_path,First_file_name),movie_path) 
@@ -31,18 +37,15 @@ def serial_movie_founder():
             continue
 # serial
         elif len(File_list)!=1:
-            Serial_root ,ext= os.path.splitext(File_list[0]).lower()
-            s_index=Serial_root.find(".s")
-            serial_name=Serial_root[:s_index]
-            serial_path=os.path.join(main_path,serial_name)
-            if not os.path.exists(serial_path):
-                os.makedirs(serial_path)
-            for episodes in File_list:
-                  shutil.move(os.path.join(main_path,episodes),serial_path)
-            File_list.clear()
-# end the process 
-        if len(filenames)==0:
-            break
-        else:
-            continue
+                Serial_root ,ext= os.path.splitext(File_list[0])
+                s_index=Serial_root.lower().find(".s")
+                serial_name=Serial_root[:s_index]
+                serial_path=os.path.join(main_path,serial_name)
+                if not os.path.exists(serial_path):
+                    os.makedirs(serial_path)
+                for episodes in File_list:
+                        shutil.move(os.path.join(main_path,episodes),serial_path)
+                File_list.clear()
+        
+        continue
 serial_movie_founder()
